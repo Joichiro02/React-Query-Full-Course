dff496ac4e89ad03925e41bbfb02ca5d3e70015c
markanthonyfamarin <markanthonyfamarin@gmail.com>
2022-11-11 20:46:29 +0800

    fetch data when trigger a click event

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -7,15 +7,16 @@ const fetchSuperHeroes = () => {
 }
 
 const RQSuperHeroesPage = () => {
-    const { data, isLoading, isError, error, isFetching } = useQuery(
+    const { data, isLoading, isError, error, isFetching, refetch } = useQuery(
         "super-heroes",
         fetchSuperHeroes,
         // { cacheTime: 5000 }, //default 5mins
         // { staleTime: 30000 }, //default 0 //after 30seconds will fetch again the data 
         // { refetchOnMount: true }, //default true //the query will REFETCH ON MOUNT if the data is STALE //if {refetchOnMount: "always"} even the data is not in STALE state if will fetch data 
         // { refetchOnWindowFocus: true }, //default true //every time that the tapgit/window is on focos the background refetch will be initiated //if {refetchOnWindowFocus: "always"} even the data is not in STALE state if will fetch data 
-        { refetchInterval: true }, //default false // this is fetch data infinite it use for the website that need to display data every single time, if the value is true every time that the browser is in focus it will fetch data // {refetchInterval: 2000} can use time, that means if the value is 2seconds, every 2seconds it will fetch data
-        { refetchIntervalInBackground: false } //it will continue to pull data even the browser in not focus
+        // { refetchInterval: true }, //default false // this is fetch data infinite it use for the website that need to display data every single time, if the value is true every time that the browser is in focus it will fetch data // {refetchInterval: 2000} can use time, that means if the value is 2seconds, every 2seconds it will fetch data
+        // { refetchIntervalInBackground: false }, //it will continue to pull data even the browser in not focus
+        { enabled: true } //default true //this will not fetch automatically the data, its need to be trigger the "refetch" that's why it put in onClick 
     );
 
 
@@ -28,12 +29,13 @@ const RQSuperHeroesPage = () => {
 
     console.log({ isLoading, isFetching })
 
-    if (isLoading) return <h2>Loading...</h2>
+    if (isLoading || isFetching) return <h2>Loading...</h2>
     if (isError) return <h2>{error.message}</h2>
 
     return (
         <>
             <h2 className='rq-super-heroes'>RQ Super Heroes Page</h2>
+            <button onClick={refetch}>Fetch Heros</button>
             {
                 data?.data.map(hero => (
                     <div key={hero.name}>{hero.name}</div>
