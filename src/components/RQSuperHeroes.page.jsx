20d3874ff76a9654804da888e6a72661fb7c0707
markanthonyfamarin <markanthonyfamarin@gmail.com>
2022-11-11 20:31:08 +0800

    polling data

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -11,9 +11,11 @@ const RQSuperHeroesPage = () => {
         "super-heroes",
         fetchSuperHeroes,
         // { cacheTime: 5000 }, //default 5mins
-        { staleTime: 30000 }, //default 0 //after 30seconds will fetch again the data 
-        { refetchOnMount: true }, //default true //the query will REFETCH ON MOUNT if the data is STALE //if {refetchOnMount: "always"} even the data is not in STALE state if will fetch data 
-        { refetchOnWindowFocus: true } //default true //every time that the tapgit/window is on focos the background refetch will be initiated //if {refetchOnWindowFocus: "always"} even the data is not in STALE state if will fetch data 
+        // { staleTime: 30000 }, //default 0 //after 30seconds will fetch again the data 
+        // { refetchOnMount: true }, //default true //the query will REFETCH ON MOUNT if the data is STALE //if {refetchOnMount: "always"} even the data is not in STALE state if will fetch data 
+        // { refetchOnWindowFocus: true }, //default true //every time that the tapgit/window is on focos the background refetch will be initiated //if {refetchOnWindowFocus: "always"} even the data is not in STALE state if will fetch data 
+        { refetchInterval: true }, //default false // this is fetch data infinite it use for the website that need to display data every single time, if the value is true every time that the browser is in focus it will fetch data // {refetchInterval: 2000} can use time, that means if the value is 2seconds, every 2seconds it will fetch data
+        { refetchIntervalInBackground: false } //it will continue to pull data even the browser in not focus
     );
 
 
